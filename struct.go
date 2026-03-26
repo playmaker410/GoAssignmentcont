436dc78e226e2e78cd46ceb1a6cc78b0d7ce2358
playmaker410 <odinakaoshim@gmail.com>
2026-03-26 09:48:40 +0000

    completed version

--- struct.go
+++ struct.go
@@ -17,7 +17,7 @@ var student3 Student
 
 student1.Name = "Joshua OKonkwo"
 student1.Age = 70
-student.Score =  100
+student1.Score =  100
 
 
 //specifing his own
@@ -30,6 +30,22 @@ student2.Score = 80
 
 student3.Name = "Angle Nnamani"
 student3.Age = 50
-student.Score = 80
+student3.Score = 80
 
+fmt.Println(highestScore(student1,student2,student3).Name)
+fmt.Println(highestScore(student1,student2,student3).Score)
+}
+
+func  highestScore(student1 Student,student2 Student,student3 Student)Student{
+maxScore := student1
+
+if student2.Score > maxScore.Score{
+maxScore = student2
+}
+
+if  student3.Score > maxScore.Score{
+maxScore = student3
+}
+
+return maxScore
 }
