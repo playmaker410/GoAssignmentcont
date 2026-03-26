bda8cf43d54c20df61e56f2b067a6a2e7e2c8c62
playmaker410 <odinakaoshim@gmail.com>
2026-03-26 12:02:53 +0000

    class record that still neeed debug

---
+++ StudentRecord.go
@@ -0,0 +1,91 @@
+package main
+import (
+	"fmt"
+	)
+
+
+
+type Student struct{
+name string
+age int
+score int
+}
+
+func  Grades(score int) string {
+if score >= 70{
+return "A"
+} else if score >= 60{
+return "B"
+}else if score >= 50{
+return "C"
+} else{
+return "F"
+}
+}
+
+func Evendeterminer(score int) string{
+if score % 2 == 0{
+return "Even"
+}
+return "Odd"
+}
+
+func highestScore(students []Student)Student{
+top := students[0]
+
+for _, s := range students{
+if s.score > top.score{
+top = s
+}
+}
+return top
+
+}
+
+func averageScore(students []Student)float64{
+total := 0
+for _, s := range students{
+total += s.score 
+}
+return float64(len(students)) /
+float64(len(students))
+
+}
+}
+
+
+func main(){
+Students := []Student{
+{"JOHN", 18,75},
+{"PETER", 17,62},
+{"DIVINE", 19,48},
+{"DANIEL", 18,55},
+{"PAUL", 20, 80},
+}
+
+for _, s := range students {
+fmt.Println("Name:", s.name)
+fmt.Println("Age",s.age)
+fmt.Println("Score", s.age)
+fmt.Println("Grade:",Grade(s.score))
+fmt.Println("-----------------------")
+
+}
+
+
+top := averageScore(students)
+fmt.Println("Average score", avg)
+
+
+studentMap := make(map[string]Student)
+for _, s := range students {
+studentMap[s.name]= s
+}
+name : "mary"
+if student, found := studentMap[name];
+found {
+fmt.Println("Found:", student)
+} else{
+fmt.Println("student not found")
+}
+}
